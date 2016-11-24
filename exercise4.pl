e98910e5728391b2ca62a7df21b30eabcdf8f7ef
linux <linux@linux-ElementaryOS.(none)>
2016-11-24 11:24:45 +0800

    a

---
+++ exercise4.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+my@bases = ('A','C','G','T');
+print "Here are the array elements:";
+print $bases[0];
+print "\nFirst element: ";
+print $bases[1];
+print "\nSecond element: ";
+print $bases[2];
+print "\nThird element: ";
+print $bases[3];
+
+
+
