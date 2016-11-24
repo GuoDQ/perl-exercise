90003317ab4fbc15703566b2df02539f0bd3b3d1
linux <linux@linux-ElementaryOS.(none)>
2016-11-24 09:27:08 +0800

    connect

---
+++ connectDNA.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -l
+use strict;
+use warnings;
+my $DNA1 = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+my $DNA2 = 'ATAGTGCCGTGAGAGTGATGTAGTA';
+print "Here are the original two DNA fragments:\n\n";
+print $DNA1,"\n";
+print $DNA2,"\n";
+my $DNA3 = "$DNA1$DNA2";
+print "Here is the concatenation of the first two fragments (version 1):\n\n";
+print "$DNA3\n\n";
+exit;
